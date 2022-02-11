1d280bc6380efb4f9625968c7208460d306af36c
baky2708 <flavioisaque123@gmail.com>
2022-02-11 19:55:58 -0300

    feat: add help and version

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,5 @@
 local options = require('src.options')
+local version = 22.2
 
 if arg[1] == '-o' then
 	options.open()
@@ -10,4 +11,24 @@ end
 
 if arg[1] == '-r' then
 	options.react()
+end
+
+if arg[1] == '-v' or arg[1] == '-V' then
+	print(version)
+end
+
+if not arg[1] or arg[1] == '-h' then
+	os.execute('clear')
+	print()
+	print('Use the arguments')
+	print()
+	print('\'lua workflow -[first_arg] --[secound_arg]\'')
+	print()
+	print('     first_arg: ')
+	print('          -d : to create default workflow')
+	print('          -r : to create react workflow')
+	print()
+	print('     secound_arg')
+	print('          --jest : to create workflow with jest pane')
+	print('          --cypress : to create workflow with cypress pane')
 end
